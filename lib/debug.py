a33b4ad8ad0ff890fa558ced83f2502f212955e9
Bennet Owino <bennetkowin09@gmail.com>
2025-03-09 02:46:34 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from email_address_parser import EmailAddressParser
 
 if __name__ == '__main__':
+    from email_address_parser import EmailAddressParser
     import ipdb; ipdb.set_trace()
--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Use regex to split by commas or spaces
+        emails = re.split(r'[,\s]+', self.email_addresses)
+        
+        # Remove empty strings, get unique emails, and sort alphabetically
+        return sorted(set(filter(None, emails)))
+
